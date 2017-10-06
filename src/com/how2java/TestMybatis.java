d89d596785978dfc5eec112bfd1155fd664d3ddf
liuxueke <xueke.liu@outlook.com>
2017-10-06 22:34:33 +0800

    2017-10-6 22:34:30

--- src/com/how2java/TestMybatis.java
+++ src/com/how2java/TestMybatis.java
@@ -1,8 +1,10 @@
 package com.how2java;
- 
+  
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
  
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -10,26 +12,26 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
  
 import com.how2java.pojo.Category;
- 
+  
 public class TestMybatis {
- 
+  
     public static void main(String[] args) throws IOException {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession session = sqlSessionFactory.openSession();
- 
-        listAll(session);
+  
+        Map<String,Object> params = new HashMap<>();
+        params.put("id", 3);
+        params.put("name", "cat");
          
-        session.commit();
-        session.close();
- 
-    }
- 
-    private static void listAll(SqlSession session) {
-        List<Category> cs = session.selectList("listCategory");
+        List<Category> cs = session.selectList("listCategoryByIdAndName",params);
         for (Category c : cs) {
             System.out.println(c.getName());
         }
+ 
+        session.commit();
+        session.close();
+  
     }
 }
